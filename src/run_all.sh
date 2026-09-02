1cfa4a8b6598bbadb82aeb2f61071d2ee787392f
ghrgriner <rgriner_fwd@outlook.com>
2026-09-02 10:39:54 -0500

    Add `tr_revisions.txt` file to output
    This stores the revision ids of pages where `has_trans == 'Y'`

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,4 +1,6 @@
+set -xv
 # First three files should be run in the order listed
 python3 run_extract_tr_long.py > run_extract_tr_long.log
 python3 run_tr_long_to_wide.py
 python3 run_trans_stats_dib.py > run_trans_stats_dib.log
+python3 run_extract_revisions.py
